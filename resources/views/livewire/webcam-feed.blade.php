0d3c561deb044c5d22238b986be9794bb3937450
FacuM <facumo.fm@gmail.com>
2023-03-25 03:49:46 -0300

    cameras: unload unused streams

--- resources/views/livewire/webcam-feed.blade.php
+++ resources/views/livewire/webcam-feed.blade.php
@@ -1,4 +1,7 @@
 <div {{-- wire:poll.5s='refreshFeed' --}}>
     {{-- <img src="{{ $snapshot }}" /> --}}
-    <img src="{{ $url }}" style="transform: scale(1, 1) rotate(0deg); width: 100%; height: 100%; opacity: 1;" />
+    <img
+        data-src="{{ $url }}"
+        style="transform: scale(1, 1) rotate(0deg); width: 100%; height: 100%; opacity: 1;"
+    />
 </div>
