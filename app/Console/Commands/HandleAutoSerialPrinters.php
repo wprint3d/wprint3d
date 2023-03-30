01729b56e15c486a01a6e4f1eea424da89ca9c67
FacuM <facumo.fm@gmail.com>
2023-03-29 21:27:13 -0300

    HandleAutoSerialPrinters: move to a separate service

--- app/Console/Commands/HandleAutoSerialPrinters.php
+++ app/Console/Commands/HandleAutoSerialPrinters.php
@@ -45,86 +45,98 @@ class HandleAutoSerialPrinters extends Command
     {
         $log = Log::channel('printers-poller');
 
-        $minPollIntervalSecs = Configuration::get('lastSeenPollIntervalSecs', env('PRINTER_LAST_SEEN_POLL_INTERVAL_SECS'));
+        $minPollIntervalSecs    = Configuration::get('lastSeenPollIntervalSecs', env('PRINTER_LAST_SEEN_POLL_INTERVAL_SECS'));
+        $commandTimeoutSecs     = Configuration::get('commandTimeoutSecs',       env('PRINTER_COMMAND_TIMEOUT_SECS'));
+        $autoSerialIntervalsecs = Configuration::get('autoSerialIntervalsecs',   env('PRINTER_AUTO_SERIAL_INTERVAL_SECS'));
 
-        $commandTimeoutSecs = Configuration::get('commandTimeoutSecs', env('PRINTER_COMMAND_TIMEOUT_SECS'));
+        $lastLoop = time();
 
-        foreach (Printer::cursor() as $printer) {
-            if ($printer->activeFile) { continue; }
-
-            if (!Serial::nodeExists( $printer->node )) {
-                PrinterConnectionStatusUpdated::dispatch( $printer->_id );
+        while (true) {
+            if (time() - $lastLoop < $autoSerialIntervalsecs) {
+                sleep(1);
 
                 continue;
             }
 
-            $serial = new Serial(
-                fileName:  $printer->node,
-                baudRate:  $printer->baudRate,
-                timeout:   $commandTimeoutSecs,
-                printerId: $printer->_id
-            );
+            $lastLoop = time();
 
-            try {
-                $queuedCommands = $printer->getResetQueuedCommands();
+            foreach (Printer::cursor() as $printer) {
+                if ($printer->activeFile) { continue; }
 
-                foreach ($queuedCommands as $command) {
-                    $serial->sendCommand( $command );
-                }
-
-                if ($queuedCommands) {
-                    $serial->readUntilBlank();
-
-                    $log->info( $printer->node . ': the following commands were processed: ' . Arr::join($queuedCommands, ', ') );
-                }
-
-                if (
-                    time() - $printer->lastSeen->toDateTime()->getTimestamp()
-                    >
-                    $minPollIntervalSecs
-                ) { // should update lastSeen?
-                    $response = $serial->query('M105');
-
-                    if (!Str::contains($response, 'ok') && !Str::contains($response, 'busy')) {
-                        $log->error( $printer->node . ': connection failed: ' . $response );
-
-                        $printer->setLastError( $response );
-
-                        continue;
-                    }
-
-                    $printer->setStatistics( $response, 0 );
-
-                    $statistics = $printer->getStatistics();
-
-                    foreach (array_keys($statistics['extruders']) as $extruderIndex) {
-                        if ($extruderIndex == 0) continue;
-
-                        $printer->setStatistics( $serial->query('M105 T' . $extruderIndex), $extruderIndex );
-                    }
-
-                    $log->debug('OK: ' . $response);
-
-                    $printer->lastSeen = new UTCDateTime();
-                    $printer->save();
-
+                if (!Serial::nodeExists( $printer->node )) {
                     PrinterConnectionStatusUpdated::dispatch( $printer->_id );
-                }
-            } catch (Exception $exception) {
-                $printer->setLastError( $exception->getMessage() );
 
-                $log->error(
-                    $printer->node . ': connection failed: ' . $exception->getMessage() . PHP_EOL .
-                    PHP_EOL .
-                    $exception->getTraceAsString()
+                    continue;
+                }
+
+                $serial = new Serial(
+                    fileName:  $printer->node,
+                    baudRate:  $printer->baudRate,
+                    timeout:   $commandTimeoutSecs,
+                    printerId: $printer->_id
                 );
 
-                PrinterConnectionStatusUpdated::dispatch( $printer->_id );
+                try {
+                    $queuedCommands = $printer->getResetQueuedCommands();
 
-                continue;
+                    foreach ($queuedCommands as $command) {
+                        $serial->sendCommand( $command );
+                    }
+
+                    if ($queuedCommands) {
+                        $serial->readUntilBlank();
+
+                        $log->info( $printer->node . ': the following commands were processed: ' . Arr::join($queuedCommands, ', ') );
+                    }
+
+                    if (
+                        time() - $printer->lastSeen->toDateTime()->getTimestamp()
+                        >
+                        $minPollIntervalSecs
+                    ) { // should update lastSeen?
+                        $response = $serial->query('M105');
+
+                        if (!Str::contains($response, 'ok') && !Str::contains($response, 'busy')) {
+                            $log->error( $printer->node . ': connection failed: ' . $response );
+
+                            $printer->setLastError( $response );
+
+                            continue;
+                        }
+
+                        $printer->setStatistics( $response, 0 );
+
+                        $statistics = $printer->getStatistics();
+
+                        foreach (array_keys($statistics['extruders']) as $extruderIndex) {
+                            if ($extruderIndex == 0) continue;
+
+                            $printer->setStatistics( $serial->query('M105 T' . $extruderIndex), $extruderIndex );
+                        }
+
+                        $log->debug('OK: ' . $response);
+
+                        $printer->lastSeen = new UTCDateTime();
+                        $printer->save();
+
+                        PrinterConnectionStatusUpdated::dispatch( $printer->_id );
+                    }
+                } catch (Exception $exception) {
+                    $printer->setLastError( $exception->getMessage() );
+
+                    $log->error(
+                        $printer->node . ': connection failed: ' . $exception->getMessage() . PHP_EOL .
+                        PHP_EOL .
+                        $exception->getTraceAsString()
+                    );
+
+                    PrinterConnectionStatusUpdated::dispatch( $printer->_id );
+
+                    continue;
+                }
             }
         }
 
-        return Command::SUCCESS;
+        return Command::FAILURE;
     }
 }
