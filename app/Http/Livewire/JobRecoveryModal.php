3b535f3f933aecebc15d008ba5c75d7f3ee79ace
FacuM <facumo.fm@gmail.com>
2023-04-01 00:04:28 -0300

    JobRecoveryModal: add offsets and support logic for a safer resting position

--- app/Http/Livewire/JobRecoveryModal.php
+++ app/Http/Livewire/JobRecoveryModal.php
@@ -42,6 +42,13 @@ class JobRecoveryModal extends Component
 
     const SERIAL_COMMAND_TIMEOUT_SECS = 3;
 
+    const E_AXIS_RETRACT_OFFSET   =  5;   // mm
+    const X_AXIS_HOLDING_OFFSET   = -1;   // mm
+    const Y_AXIS_HOLDING_OFFSET   = -1;   // mm
+    const Z_AXIS_HOLDING_OFFSET   =  1.5; // mm
+
+    const XY_AXIS_WITH_MIN_HOLDING_OFFSET = 1; // mm
+
     public function boot() {
         $this->baseFilesDir = env('BASE_FILES_DIR');
 
@@ -131,19 +138,25 @@ class JobRecoveryModal extends Component
             sleep(1);
         }
 
-        $warmUpCommands   = [];
-        $preSetUpCommands = [];
-        $setUpCommands    = [];
+        $warmUpCommands     = [];
+        $preSetUpCommands   = [];
+        $setUpCommands      = [];
+        $postSetUpCommands  = [];
 
         // default movement mode for Marlin is absolute
         $lastMovementMode = 'G90';
 
-        $absolutePosition = [ 'x' => null, 'y' => null, 'z' => null ];
+        $previousPosition = [ 'x' => null, 'y' => null, 'z' => null ];
+        $absolutePosition = $previousPosition;
+
+        $minLayerPositionXY = [ 'x' => null, 'y' => null ];
 
         $gcode = Storage::get( $this->baseFilesDir . '/' . $this->printer->activeFile );
         $gcode = explode(PHP_EOL, $gcode);
 
         foreach ($gcode as $index => $line) {
+            $previousPosition = $absolutePosition;
+
             if ($line == 'G90' || $line == 'G91') {
                 $lastMovementMode = $line;
             } else if (
@@ -184,6 +197,21 @@ class JobRecoveryModal extends Component
                 $setUpCommands[] = $line;
             }
 
+            if ($absolutePosition['x'] !== null && $absolutePosition['y'] !== null) {
+                if ($minLayerPositionXY['x'] === null || $absolutePosition['x'] < $minLayerPositionXY['x']) {
+                    $minLayerPositionXY['x'] = $absolutePosition['x'];
+                }
+
+                if ($minLayerPositionXY['y'] === null || $absolutePosition['y'] < $minLayerPositionXY['y']) {
+                    $minLayerPositionXY['y'] = $absolutePosition['y'];
+                }
+            }
+
+            // Reset on layer change
+            if ($absolutePosition['z'] != $previousPosition['z']) {
+                $minLayerPositionXY = [ 'x' => null, 'y' => null ];
+            }
+
             if ($index == $this->printer->lastLine) { break; }
         }
 
@@ -197,24 +225,61 @@ class JobRecoveryModal extends Component
             return;
         }
 
-        $preSetUpCommands[] = 'G91';    // relative mode
-        $preSetUpCommands[] = 'G0 Z1';  // make the nozzle go up
-        $preSetUpCommands[] = "M109 R{$jobRestorationHomingTemperature}"; // wait for hotend cooldown
+        $preSetUpCommands[] = 'G91';                                        // relative mode
+        $preSetUpCommands[] = 'G1 E-' . self::E_AXIS_RETRACT_OFFSET;        // retract N mm
+        $preSetUpCommands[] = 'G0 Z'  . self::Z_AXIS_HOLDING_OFFSET;        // make the nozzle go up
+        $preSetUpCommands[] = "M109 R{$jobRestorationHomingTemperature}";   // wait for hotend cooldown
 
-        $startZ = $absolutePosition['z'] + 1;
+        /*
+         * This is gonna be the default target position, unless a minimum
+         * position is known.
+         */
+        $startPos = [
+            'x' => $absolutePosition['x'] + self::X_AXIS_HOLDING_OFFSET,
+            'y' => $absolutePosition['x'] + self::Y_AXIS_HOLDING_OFFSET,
+            'z' => $absolutePosition['z'] + self::Z_AXIS_HOLDING_OFFSET
+        ];
+
+        $log->debug('$startPos: defaults prepared: ' . json_encode( $startPos ));
+
+        /*
+         * If a minimum position is known, we're gonna try to go from $startPos
+         * and offset that by XY_AXIS_WITH_MIN_HOLDING_OFFSET on both X and Y
+         * axis.
+         */
+        if (
+            $minLayerPositionXY['x'] !== null && $minLayerPositionXY['y'] !== null
+            &&
+            $startPos['x'] + self::XY_AXIS_WITH_MIN_HOLDING_OFFSET > $minLayerPositionXY['x']
+            &&
+            $startPos['y'] - self::XY_AXIS_WITH_MIN_HOLDING_OFFSET > $minLayerPositionXY['y']
+        ) {
+            $startPos['x'] += self::XY_AXIS_WITH_MIN_HOLDING_OFFSET;
+            $startPos['y'] -= self::XY_AXIS_WITH_MIN_HOLDING_OFFSET;
+
+            $log->debug('$startPos: minimum layer XY available, decreasing XY to a safer resting position: ' . json_encode( $startPos ) . ', minimum is: ' . json_encode( $minLayerPositionXY ));
+        }
 
         $setUpCommands[] = 'G28'; // auto-home all axis
         $setUpCommands[] = 'G90'; // absolute mode
-        $setUpCommands[] = "G0  Z{$startZ}";                                          // move to target Z + 1mm
-        $setUpCommands[] = "G0  X{$absolutePosition['x']} Y{$absolutePosition['y']}"; // move to target X/Y
-        $setUpCommands[] = "G0  Z{$absolutePosition['z']}";                           // move to target Z
-        $setUpCommands[] = $lastMovementMode;
+        $setUpCommands[] = "G0 X{$startPos['x']} Y{$startPos['y']} Z{$startPos['z']}"; // move to target X/Y/Z + offset
+        $setUpCommands[] = "G0 Z{$absolutePosition['z']}";                             // move to target Z
+
+        $postSetUpCommands[] = "G0 X{$absolutePosition['x']} Y{$absolutePosition['y']}"; // move to target X/Y
+        $postSetUpCommands[] = 'G1 E' . self::E_AXIS_RETRACT_OFFSET;                     // de-retract N mm
+        $postSetUpCommands[] = $lastMovementMode;
+
+        $log->debug('warmUpCommands: '    . json_encode( $warmUpCommands    ));
+        $log->debug('preSetUpCommands: '  . json_encode( $preSetUpCommands  ));
+        $log->debug('setUpCommands: '     . json_encode( $setUpCommands     ));
+        $log->debug('postSetUpCommands: ' . json_encode( $postSetUpCommands ));
 
         $gcode = array_merge(
             $warmUpCommands,
             $preSetUpCommands,
             $setUpCommands,
             $warmUpCommands,
+            $postSetUpCommands,
             array_splice(
                 array:  $gcode,
                 offset: $this->printer->lastLine
