e968c6c4880d98baf2eddbfaa154c4b47db3ba0e
FacuM <facumo.fm@gmail.com>
2023-04-02 20:52:27 -0300

    Events: cast synchronously

--- app/Events/PrinterConnectionStatusUpdated.php
+++ app/Events/PrinterConnectionStatusUpdated.php
@@ -6,18 +6,16 @@ use App\Models\Printer;
 
 use Exception;
 
-use Illuminate\Broadcasting\Channel;
 use Illuminate\Broadcasting\InteractsWithSockets;
-use Illuminate\Broadcasting\PresenceChannel;
 use Illuminate\Broadcasting\PrivateChannel;
 
-use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
 
 use Illuminate\Foundation\Events\Dispatchable;
 
 use Illuminate\Queue\SerializesModels;
 
-class PrinterConnectionStatusUpdated implements ShouldBroadcast
+class PrinterConnectionStatusUpdated implements ShouldBroadcastNow
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
--- app/Events/PrintersMapUpdated.php
+++ app/Events/PrintersMapUpdated.php
@@ -5,13 +5,13 @@ namespace App\Events;
 use Illuminate\Broadcasting\Channel;
 use Illuminate\Broadcasting\InteractsWithSockets;
 
-use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
 
 use Illuminate\Foundation\Events\Dispatchable;
 
 use Illuminate\Queue\SerializesModels;
 
-class PrintersMapUpdated implements ShouldBroadcast
+class PrintersMapUpdated implements ShouldBroadcastNow
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
