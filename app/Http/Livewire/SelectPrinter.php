1bef5652a49596d05750a595fe13f4e28af294e0
FacuM <facumo.fm@gmail.com>
2023-05-15 00:29:54 -0300

    Remove dangerous force reload on first printer detection

--- app/Http/Livewire/SelectPrinter.php
+++ app/Http/Livewire/SelectPrinter.php
@@ -17,23 +17,7 @@ class SelectPrinter extends Component
     public ?Collection  $printers;
     public ?Printer     $printer;
 
-    public function boot() {
-        $user = Auth::user();
-
-        $this->printerId = $user->activePrinter;
-        $this->printers  = Printer::select('_id', 'node', 'machine.machineType', 'machine.uuid')->get();
-
-        if ($this->printerId && !Printer::find( $this->printerId )) {
-            $this->printerId = null;
-        }
-
-        if (!$this->printerId && filled( $this->printers )) {
-            $this->printerId = $this->printers[0]->_id;
-
-            $user->activePrinter = $this->printerId;
-            $user->save();
-        }
-    }
+    protected $listeners = [ 'hardwareChangeDetected' => '$refresh' ];
 
     public function change() {
         $this->printer = Printer::find( $this->printerId );
@@ -49,6 +33,20 @@ class SelectPrinter extends Component
 
     public function render()
     {
+        $user = Auth::user();
+
+        $this->printerId = $user->activePrinter;
+        $this->printers  = Printer::select('_id', 'node', 'machine.machineType', 'machine.uuid')->get();
+
+        if ($this->printerId && !Printer::find( $this->printerId )) {
+            $this->printerId = null;
+        }
+
+        if (!$this->printerId && filled( $this->printers )) {
+            $user->activePrinter = $this->printers[0]->_id;
+            $user->save();
+        }
+
         return view('livewire.select-printer');
     }
 }
