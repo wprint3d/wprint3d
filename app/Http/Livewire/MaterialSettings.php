7dfb26ce6a0743e9d68e849614ea929da8184502
FacuM <facumo.fm@gmail.com>
2023-06-01 23:57:32 -0300

    Add support for pre-heating materials

---
+++ app/Http/Livewire/MaterialSettings.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class MaterialSettings extends Component
+{
+    public $material;
+
+    public $name;
+    public $hotendTemperature;
+    public $bedTemperature;
+
+    public function delete() {
+        $this->material->delete();
+
+        $this->emit('materialsChanged');
+    }
+
+    public function updated($name, $value) {
+        if ($name == 'hotendTemperature' || $name == 'bedTemperature') {
+            if (!is_numeric( $value ) || $value < 0) {
+                $value = 0;
+            }
+
+            $value = (int) $value;
+
+            switch ($name) {
+                case 'hotendTemperature': $name = 'temperatures.hotend'; break;
+                case 'bedTemperature':    $name = 'temperatures.bed';    break;
+            }
+        } else if ($name == 'name' && !filled( trim($value) )) {
+            $value = null;
+        }
+
+        $this->material->update([ $name => $value ]);
+
+        $this->emit('materialsChanged');
+    }
+
+    public function render()
+    {
+        $this->name              = $this->material->name;
+        $this->hotendTemperature = $this->material->temperatures['hotend'];
+        $this->bedTemperature    = $this->material->temperatures['bed'];
+
+        if (!$this->name) {
+            $this->name = 'Unknown';
+        }
+
+        return view('livewire.material-settings');
+    }
+}
---
+++ resources/views/livewire/settings-modal-materials.blade.php
@@ -0,0 +1,23 @@
+<div>
+    <div class="row text-start">
+        @if ($materials->count())
+            @foreach ($materials as $material)
+                @livewire('material-settings', [ 'material' => $material ], key( $material->_id ))
+            @endforeach
+
+            <div class="col-12 mt-2 d-flex justify-content-center">
+                <button wire:click="add" class="btn btn-primary"> @svg('plus') Add more </button>
+            </div>
+        @else
+            <div class="d-flex align-items-center">
+                <div class="d-flex flex-column flex-fill align-items-center mt-3">
+                    @svg('gear-fill', [ 'class' => 'fs-1' ])
+
+                    <p class="text-center mt-3"> No materials are configured, add one or more materials in order to enable temperature presets. </p>
+
+                    <button wire:click="add" class="btn btn-primary"> Add your first material </button>
+                </div>
+            </div>
+        @endif
+    </div>
+</div>
