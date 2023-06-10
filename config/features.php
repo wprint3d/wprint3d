36a780f2a6a1e161d262082851d74b8fdbe0e082
FacuM <facumo.fm@gmail.com>
2023-06-10 02:30:24 -0300

    Add support for feature flags

---
+++ config/features.php
@@ -0,0 +1,12 @@
+<?php
+
+/*
+ * The array presented below contains a set of feature-flags that'll be toggled
+ * as the more advanced features are better tested.
+ * 
+ * This logic allows for the logical structures to co-exist with the rest of
+ * tested modules and lets developers turn them off and on to keep working on
+ * them locally.
+ */
+
+return [];
