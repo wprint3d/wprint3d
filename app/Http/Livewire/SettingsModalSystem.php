dd710e09c28855cec1296a88b325ea17a6bcccf6
FacuM <facumo.fm@gmail.com>
2023-11-20 01:01:02 -0300

    SettingsModalSystem: fix missing property "writeable"

--- app/Http/Livewire/SettingsModalSystem.php
+++ app/Http/Livewire/SettingsModalSystem.php
@@ -6,8 +6,12 @@ use Livewire\Component;
 
 class SettingsModalSystem extends Component
 {
+
+    public $writeable = false;
+
     public function render()
     {
         return view('livewire.settings-modal-system');
     }
+
 }
