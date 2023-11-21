772baafc2219e7bc06de703e52ea5bfa4c763931
FacuM <facumo.fm@gmail.com>
2023-11-20 23:20:27 -0300

    SettingsModalPrinterCard: display connection status

--- resources/views/livewire/settings-modal-printer-card.blade.php
+++ resources/views/livewire/settings-modal-printer-card.blade.php
@@ -1,7 +1,18 @@
 <div class="card col-12 col-md-6 col-lg-4 mt-2">
     <img src="images/printer.jpg" class="card-img-top mt-3" alt="{{ $printer->machine['machineType'] ?? 'Unknown printer'  }}">
     <div class="card-body">
+        <div class="d-flex justify-content-center mb-2">
+            <span class="badge bg-{{ $printer->connected ? 'success' : 'danger' }}">
+                @if ($printer->connected)
+                    Connected
+                @else
+                    Disconnected
+                @endif
+            </span>
+        </div>
+
         <h5 class="card-title"> {{ $printer->machine['machineType'] ?? 'Unknown printer' }} </h5>
+
         <p class="card-text">
             <ul>
             @foreach ($printer->machine as $key => $value)
