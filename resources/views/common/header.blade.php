95c9504ea34224521fe5ebcec9b9a047784b18a9
FacuM <facumo.fm@gmail.com>
2024-01-12 22:22:22 -0300

    login: animate assets loading and busy states
    The technical reason behind this change is that I just think that it looks really cool.

--- resources/views/common/header.blade.php
+++ resources/views/common/header.blade.php
@@ -5,6 +5,9 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     @vite([ 'resources/js/app.js', 'resources/css/app.scss' ])
+
+    @stack('head')
+
     @livewireStyles
 </head>
 
--- resources/views/livewire/login.blade.php
+++ resources/views/livewire/login.blade.php
@@ -1,51 +1,119 @@
+@push('head')
+    <style> .rollable { transition: max-height .5s ease-in-out; } </style>
+@endpush
+
 <div>
     <div class="d-flex flex-column min-vh-100 justify-content-center align-items-center">
-        <form wire:submit="submit" class="col-11 col-sm-8 col-md-6 col-lg-4 col-xxl-3 bg-body d-flex flex-column p-4 rounded rounded-3">
-            <div class="mb-3">
-                <label class="form-label">Username or email address</label>
-                <input wire:model="identifier" type="text" class="form-control" aria-describedby="emailHelp">
-                <div id="emailHelp" class="form-text">
-                    The default username is <b>{{ CreateSampleUser::SAMPLE_USER_NAME }}</b>.
+        <div class="animate__animated animate__slow animate__fadeIn px-4 py-1 my-1 text-center">
+            <h1 class="d-none d-md-block display-4 fw-bold text-body-emphasis">
+                {{ env('APP_NAME') }}
+            </h1>
+
+            <h1 class="d-block d-md-none display-1 fw-bold text-body-emphasis">
+                {{ env('APP_NAME') }}
+            </h1>
+
+            <div class="rollable col-auto mx-auto" style="max-height: 100vh;">
+                <div id="spinner" class="spinner-border" role="status">
+                    <span class="visually-hidden"> Loading assets... </span>
                 </div>
             </div>
 
-            <div class="mb-3">
-                <label class="form-label">Password</label>
-                <input wire:model="password" type="password" class="form-control" aria-describedby="passwordHelp">
-                <div id="passwordHelp" class="form-text">
-                    The default passsword is <b>{{ CreateSampleUser::SAMPLE_USER_PASSWORD }}</b>.
-                </div>
+            <div class="rollable col-auto mx-auto overflow-hidden" style="max-height: 0;">
+                <p class="lead mb-4">
+                    Welcome back! You can now log in to your account.
+                </p>
             </div>
+        </div>
 
-            @if (enabled( 'login.remember_me' ))
-                <div class="mb-2 form-check">
-                    <input wire:model="rememberMe" type="checkbox" class="form-check-input">
-
-                    <label class="form-check-label">
-                        Remember me on this device
-                    </label>
+        <form wire:submit="submit" class="rollable col-11 col-sm-8 col-md-6 col-lg-4 col-xxl-3 bg-body rounded rounded-3 overflow-hidden" style="max-height: 0;">
+            <div class="d-flex flex-column p-4">
+                <div class="mb-3">
+                    <label class="form-label">Username or email address</label>
+                    <input wire:model="identifier" type="text" class="form-control" aria-describedby="emailHelp">
+                    <div id="emailHelp" class="form-text">
+                        The default username is <b>{{ CreateSampleUser::SAMPLE_USER_NAME }}</b>.
+                    </div>
                 </div>
-            @endif
 
-            <span class="text-danger mb-3 text-center text-danger">
-                @error('identifier') {{ $message }} @if ($logoutReason && $logoutReason != LogoutReason::USER_REQUEST) <br> @endif <br> @enderror
-                @error('password')   {{ $message }} @if ($logoutReason && $logoutReason != LogoutReason::USER_REQUEST) <br> @endif <br> @enderror
-
-                @switch ($logoutReason)
-                    @case (LogoutReason::ACCOUNT_CHANGED)
-                        Your account settings have changed and you've been logged out, please try to log in again. <br>
-                        <br>
-                        If you can't log in, contact your system administrator for help.
-
-                        @break
-                @endswitch
+                <div class="mb-3">
+                    <label class="form-label">Password</label>
+                    <input wire:model="password" type="password" class="form-control" aria-describedby="passwordHelp">
+                    <div id="passwordHelp" class="form-text">
+                        The default passsword is <b>{{ CreateSampleUser::SAMPLE_USER_PASSWORD }}</b>.
+                    </div>
+                </div>
 
                 @if (enabled( 'login.remember_me' ))
-                    @error('rememberMe')  {{ $message }} <br> @enderror
-                @endif
-            </span>
+                    <div class="mb-2 form-check">
+                        <input wire:model="rememberMe" type="checkbox" class="form-check-input">
 
-            <button type="submit" class="btn btn-primary"> Submit </button>
+                        <label class="form-check-label">
+                            Remember me on this device
+                        </label>
+                    </div>
+                @endif
+
+                <span class="text-danger mb-3 text-center text-danger">
+                    @error('identifier') {{ $message }} @if ($logoutReason && $logoutReason != LogoutReason::USER_REQUEST) <br> @endif <br> @enderror
+                    @error('password')   {{ $message }} @if ($logoutReason && $logoutReason != LogoutReason::USER_REQUEST) <br> @endif <br> @enderror
+
+                    @switch ($logoutReason)
+                        @case (LogoutReason::ACCOUNT_CHANGED)
+                            Your account settings have changed and you've been logged out, please try to log in again. <br>
+                            <br>
+                            If you can't log in, contact your system administrator for help.
+
+                            @break
+                    @endswitch
+
+                    @if (enabled( 'login.remember_me' ))
+                        @error('rememberMe')  {{ $message }} <br> @enderror
+                    @endif
+                </span>
+
+                <button type="submit" class="btn btn-primary"> Submit </button>
+            </div>
         </form>
     </div>
 </div>
+
+@push('scripts')
+<script>
+
+const getTransitionDurationMillis = element => (
+    parseFloat(
+        getComputedStyle(element).transitionDuration
+    ) * 1000
+);
+
+document.addEventListener('DOMContentLoaded', () => {
+    let spinner = document.querySelector('#spinner'),
+        lead    = document.querySelector('.lead'),
+        form    = document.querySelector('form');
+
+    if (spinner) {
+        spinner = spinner.parentElement;
+
+        spinner.style.maxHeight = '0px';
+        spinner.classList.add('animate__animated');
+        spinner.classList.add('animate__fadeOut');
+    }
+
+    setTimeout(() => {
+        if (lead) {
+            lead = lead.parentElement;
+
+            lead.style.maxHeight = '100vh';
+        }
+
+        if (form) {
+            form.style.maxHeight = '100vh';
+        }
+
+        spinner.remove();
+    }, getTransitionDurationMillis(spinner));
+});
+
+</script>
+@endpush
