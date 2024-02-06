a24cd54f3e6eeff9fcd77d840ecc9c0837d8de84
FacuM <facumo.fm@gmail.com>
2024-02-05 22:53:31 -0300

    MaterialSettings: fix input updates not being sent to the server

--- resources/views/livewire/material-settings.blade.php
+++ resources/views/livewire/material-settings.blade.php
@@ -2,17 +2,17 @@
     <div class="row">
         <div class="col-12 col-lg-4 col-xl-12 mt-xl-2 mb-2 mb-lg-0">
             <label class="form-label"> Type </label>
-            <input wire:model="name" type="text" class="form-control" placeholder="PLA/ABS/PETG/TPU" value="{{ $name }}">
+            <input wire:model.blur="name" type="text" class="form-control" placeholder="PLA/ABS/PETG/TPU" value="{{ $name }}">
         </div>
 
         <div class="col-12 col-lg-3 col-xl-12 mt-xl-2 mb-2 mb-lg-0">
             <label class="form-label"> Hotend temperature (°C) </label>
-            <input wire:model="hotendTemperature" type="number" class="form-control" placeholder="0" value="{{ $hotendTemperature }}">
+            <input wire:model.blur="hotendTemperature" type="number" class="form-control" placeholder="0" value="{{ $hotendTemperature }}">
         </div>
 
         <div class="col-12 col-lg-3 col-xl-12 mt-xl-2">
             <label class="form-label"> Bed temperature (°C) </label>
-            <input wire:model="bedTemperature" type="number" class="form-control" placeholder="0" value="{{ $bedTemperature }}">
+            <input wire:model.blur="bedTemperature" type="number" class="form-control" placeholder="0" value="{{ $bedTemperature }}">
         </div>
 
         <div class="col-12 col-lg-2 col-xl-12 mt-xl-2 align-self-center text-center">
