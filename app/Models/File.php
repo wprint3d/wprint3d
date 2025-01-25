a08d2a24bb9141fe26f03830200d9c7a5cedc634
FacuM <facumo.fm@gmail.com>
2025-01-25 17:25:48 +0000

    PrintGcode: add layer counting and print count tracking
    - Introduces layer counting logic to track the number of layers printed.
    - Adds functionality to update printed file information in the database.
    - Includes minor refactoring and improvements to existing code.

---
+++ app/Models/File.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+use MongoDB\Laravel\Eloquent\Model;
+
+class File extends Model
+{
+    use HasFactory;
+}
