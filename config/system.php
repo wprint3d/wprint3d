b4f21d8bc218dab94299c1a552babb795e127d7f
FacuM <facumo.fm@gmail.com>
2025-01-25 17:15:41 +0000

    config: system: refactor structure
    - Updates configuration to use structured arrays with metadata
    - Improves readability and maintainability by adding descriptions, hints, and data types for each setting

--- config/system.php
+++ config/system.php
@@ -1,46 +1,211 @@
 <?php
 
 use App\Enums\BackupInterval;
+use App\Enums\DataType;
 
 return [
     'defaults' => [
 
-        'machineUUID'               => null, // string
-        'renderFileBlockingSecs'    => 60,   // seconds
+        // System
+        'machineUUID' => [
+            'value' => null,
+            'hint' => 'Machine UUID',
+            'type' => DataType::STRING,
+            'description' => 'The unique identifier for the machine.',
+            'writeable' => false,
+            'section' => 'System'
+        ],
+        'renderFileBlockingSecs' => [
+            'value' => 60,
+            'hint' => 'Render file blocking seconds',
+            'type' => DataType::INTEGER,
+            'description' => 'The time in seconds to block rendering recorded video files.',
+            'section' => 'System'
+        ],
 
         // Connection
-        'streamMaxLengthBytes'      => 256, // bytes (maximum amount of ASCII characters in a valid G-code command)
-        'negotiationWaitSecs'       => 7,   // seconds
-        'negotiationTimeoutSecs'    => 3,   // seconds
-        'negotiationMaxRetries'     => 3,   // times
-        'commandTimeoutSecs'        => 60,  // seconds
-        'runningTimeoutSecs'        => 10,  // seconds
-        'lastSeenThresholdSecs'     => 7,   // seconds
-        'lastSeenPollIntervalSecs'  => 5,   // seconds
-        'autoSerialIntervalSecs'    => 2,   // seconds
+        'streamMaxLengthBytes' => [
+            'value' => 256,
+            'hint' => 'Stream max length bytes',
+            'type' => DataType::INTEGER,
+            'description' => 'The maximum amount of ASCII characters in a valid G-code command.',
+            'section' => 'Connection'
+        ],
+        'negotiationWaitSecs' => [
+            'value' => 7,
+            'hint' => 'Negotiation delay',
+            'type' => DataType::INTEGER,
+            'description' => 'The absolute time (in seconds) that will be spent waiting for the printer to boot before trying to negotiate a connection.',
+            'section' => 'Connection'
+        ],
+        'negotiationTimeoutSecs' => [
+            'value' => 3,
+            'hint' => 'Negotiation timeout',
+            'type' => DataType::INTEGER,
+            'description' => 'The maximum time (in seconds) that will be spent setting up a printer, exceeding this time will get the printer invalidated and disabled.',
+            'section' => 'Connection'
+        ],
+        'negotiationMaxRetries' => [
+            'value' => 3,
+            'hint' => 'Negotiation retry limit',
+            'type' => DataType::INTEGER,
+            'description' => 'The maximum amount of times that we\'ll try to set up a printer, exceeding this value will get the printer invalidated and disabled.',
+            'section' => 'Connection'
+        ],
+        'commandTimeoutSecs' => [
+            'value' => 60,
+            'hint' => 'Command timeout',
+            'type' => DataType::INTEGER,
+            'description' => 'The maximum time (in seconds) that will be spent waiting for a response from the printer. Exceeding this time twice will cause the print job to be aborted.',
+            'section' => 'Connection'
+        ],
+        'runningTimeoutSecs' => [
+            'value' => 10,
+            'hint' => 'Busy check timeout',
+            'type' => DataType::INTEGER,
+            'description' => 'Some printers will stop responding after leaving the busy state. This is the maximum time (in seconds) that the printer would remain idle before we try forcing a command into it.',
+            'section' => 'Connection'
+        ],
+        'lastSeenThresholdSecs' => [
+            'value' => 7,
+            'hint' => 'Last seen threshold',
+            'type' => DataType::INTEGER,
+            'description' => 'This is the interval (in seconds) in that the printer should produce some kind of output. If exceeded, it\'ll be marked as offline.',
+            'section' => 'Connection'
+        ],
+        'lastSeenPollIntervalSecs' => [
+            'value' => 5,
+            'hint' => 'Last seen poll interval',
+            'type' => DataType::INTEGER,
+            'description' => 'This is the interval (in seconds) in which the printer will be queried for a response.',
+            'section' => 'Connection'
+        ],
+        'autoSerialIntervalSecs' => [
+            'value' => 2,
+            'hint' => 'Automatic poll interval',
+            'type' => DataType::INTEGER,
+            'description' => 'This is the interval (in seconds) in which idling printers will be reached for various automated polling operations, such as temperature, power supply status, firmware details, etc.',
+            'section' => 'Connection'
+        ],
 
-        // Limits - 1 travel unit == 1 mm or 1 in (as configured in the Printer's firmware)
-        'controlDistanceDefault'    => 10,      // travel units 
-        'controlDistanceMin'        => 1,       // travel units
-        'controlDistanceMax'        => 100,     // travel units
-        'controlFeedrateDefault'    => 1500,    // travel units per second
-        'controlFeedrateMin'        => 500,     // travel units per second
-        'controlFeedrateMax'        => 10000,   // travel units per second
-        'controlExtrusionFeedrate'  => 50,      // travel units per second
-        'controlExtrusionMinTemp'   => 170,     // celsius degress
+        // Limits
+        'controlDistanceDefault' => [
+            'value' => 10,
+            'hint' => 'Default travel distance',
+            'type' => DataType::INTEGER,
+            'description' => 'This is the default distance to move each axis (in mm) that the printer will move from the Control tab.',
+            'section' => 'Limits'
+        ],
+        'controlDistanceMin' => [
+            'value' => 1,
+            'hint' => 'Minimum travel distance',
+            'type' => DataType::INTEGER,
+            'description' => 'This is the minimum distance to move each axis (in mm) that the printer will be allowed to move from the Control tab.',
+            'section' => 'Limits'
+        ],
+        'controlDistanceMax' => [
+            'value' => 100,
+            'hint' => 'Maximum travel distance',
+            'type' => DataType::INTEGER,
+            'description' => 'This is the maximum distance to move each axis (in mm) that the printer will be allowed to move from the Control tab.',
+            'section' => 'Limits'
+        ],
+        'controlFeedrateDefault' => [
+            'value' => 1500,
+            'hint' => 'Default feedrate speed',
+            'type' => DataType::INTEGER,
+            'description' => 'This is the default speed to move each axis (in mm/s) that the printer will move from the Control tab.',
+            'section' => 'Limits'
+        ],
+        'controlFeedrateMin' => [
+            'value' => 500,
+            'hint' => 'Minimum feedrate speed',
+            'type' => DataType::INTEGER,
+            'description' => 'This is the minimum speed to move each axis (in mm/s) that the printer will be allowed to move from the Control tab.',
+            'section' => 'Limits'
+        ],
+        'controlFeedrateMax' => [
+            'value' => 10000,
+            'hint' => 'Maximum feedrate speed',
+            'type' => DataType::INTEGER,
+            'description' => 'This is the maximum speed to move each axis (in mm/s) that the printer will be allowed to move from the Control tab.',
+            'section' => 'Limits'
+        ],
+        'controlExtrusionFeedrate' => [
+            'value' => 50,
+            'hint' => 'Extrusion feedrate',
+            'type' => DataType::INTEGER,
+            'description' => 'This is the absolute speed to extrude material when requested from the Control tab.',
+            'section' => 'Limits'
+        ],
+        'controlExtrusionMinTemp' => [
+            'value' => 170,
+            'hint' => 'Minimum temperature to extrude',
+            'type' => DataType::INTEGER,
+            'description' => 'This is the minimum temperature required before being able to extrude material from the Control tab (avoids physical damage to the printer due to cold extrusion).',
+            'section' => 'Limits'
+        ],
 
-        // Miscelaneous
-        'jobBackupInterval'              => BackupInterval::EVERY_SECOND,
-        'jobStatisticsQueryIntervalSecs' => 10,                     // seconds
-        'terminalMaxLines'               => 512,                    // lines
-        'enableHaptics'                  => true,                   // boolean
+        // Miscellaneous
+        'jobBackupInterval' => [
+            'enum' => 'BackupInterval',
+            'value' => BackupInterval::EVERY_SECOND,
+            'hint' => 'Backup interval',
+            'type' => DataType::ENUM,
+            'description' => 'Specifies how frequently should we try to backup the currently active job.',
+            'section' => 'Miscellaneous'
+        ],
+        'jobStatisticsQueryIntervalSecs' => [
+            'value' => 10,
+            'hint' => 'Statistics query interval',
+            'type' => DataType::INTEGER,
+            'description' => 'This is the absolute interval (in seconds) in which the printer statistics will be queried throughout an active print job.',
+            'section' => 'Miscellaneous'
+        ],
+        'terminalMaxLines' => [
+            'value' => 512,
+            'hint' => 'Maximum terminal lines',
+            'type' => DataType::INTEGER,
+            'description' => 'This is the maximum amount of lines that can be shown in the Terminal tab. If exceeded, extra lines are removed from oldest to newest.',
+            'section' => 'Miscellaneous'
+        ],
+        'enableHaptics' => [
+            'value' => true,
+            'hint' => 'Haptic feedback',
+            'type' => DataType::BOOLEAN,
+            'description' => 'Whether to enable haptic feedback throughout the entire system. Please note that, on mobile devices, it\'ll be necessary to disable the do not disturb mode before this settings has any effect.',
+            'section' => 'Miscellaneous'
+        ],
 
         // Advanced settings
-        'debugSerial'                     => false,   // boolean
-        'enableLibCamera'                 => true,    // boolean
-        'jobRestorationTimeoutSecs'       => 30,      // seconds
-        'jobRestorationHomingTemperature' => 180,     // celsius degrees
-        'developerMode'                   => false    // boolean (toggles the Development tab in the Settings pane sidebar)
+        'debugSerial' => [
+            'value' => false,
+            'hint' => 'Debug serial transactions',
+            'type' => DataType::BOOLEAN,
+            'description' => 'Whether to enable debug logging of all kinds of transactions running through the serial protocol. This is EXTREMELY taxing for your system\'s I/O throughput and has the potential to cause slowdowns, crashes and timeouts of your print jobs on slower systems such as single-board computers.',
+            'section' => 'Advanced settings'
+        ],
+        'enableLibCamera' => [
+            'value' => true,
+            'hint' => 'Libcamera support',
+            'type' => DataType::BOOLEAN,
+            'description' => 'Whether to enable libcamera support. Generally, you\'ll want this setting enabled, however, some single-board computers\' kernels are broken and make the streaming process relatively taxing on the already scarce system resources. This setting lets you disable this feature in favor of using a USB camera or no camera at all instead.',
+            'section' => 'Advanced settings'
+        ],
+        'jobRestorationHomingTemperature' => [
+            'value' => 180,
+            'hint' => 'Job restore homing temperature',
+            'type' => DataType::INTEGER,
+            'description' => 'The absolute temperature (in celsius) that is required to cool back down to before moving back home after tapping Recover on a failed job. This avoids leaving hot plastic pieces all over the print and the bed.',
+            'section' => 'Advanced settings'
+        ],
+        'developerMode' => [
+            'value' => false,
+            'hint' => 'Enable developer mode',
+            'type' => DataType::BOOLEAN,
+            'description' => 'Whether to enable the developer mode which shows a Development tab with tools for core and plugin developers. A page reload is required to apply changes to this setting.',
+            'section' => 'Advanced settings'
+        ]
 
     ]
 ];
