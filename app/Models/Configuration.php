37625c14154b15aed957f8914481c802c28bad77
FacuM <facumo.fm@gmail.com>
2025-01-26 01:50:35 +0000

    Models: Configuration: enhance configuration management
    - Adds type validation and write protection for configuration values
    - Introduces custom exceptions for initialization errors
    - Improves default value handling in `get` method

--- app/Models/Configuration.php
+++ app/Models/Configuration.php
@@ -2,6 +2,10 @@
 
 namespace App\Models;
 
+use App\Enums\DataType;
+
+use App\Exceptions\InitializationException;
+
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 use MongoDB\Laravel\Eloquent\Model;
@@ -15,14 +19,36 @@ class Configuration extends Model
     public static function get($key, $default = null) {
         $config = self::where('key', $key)->first();
 
-        if ($config) {
-            return $config->value;
+        if (!$config) { return $default; }
+
+        return $config->value ?? $config->default ?? $default;
+    }
+
+    public static function set($key, $value): bool {
+        $config = self::where('key', $key)->first();
+
+        if (!$config) {
+            throw new InitializationException("No such configuration");
         }
 
-        $config = config("system.defaults.{$key}");
+        if (isset($config->writeable) && !$config->writeable) {
+            throw new InitializationException("The configuration key {$key} is not writeable");
+        }
 
-        if ($config !== null) { return $config; }
+        if ($config->type == DataType::BOOLEAN && !is_bool($value)) {
+            throw new InitializationException("A boolean value is expected for this configuration key");
+        } else if ($config->type == DataType::INTEGER && !is_int($value)) {
+            throw new InitializationException("An integer value is expected for this configuration key");
+        } else if ($config->type == DataType::FLOAT && !is_float($value)) {
+            throw new InitializationException("A float value is expected for this configuration key");
+        } else if ($config->type == DataType::STRING && !is_string($value)) {
+            throw new InitializationException("A string value is expected for this configuration key");
+        }
 
-        return $default;
+        $config->value = $value;
+        $config->save();
+
+        return true;
     }
+
 }
