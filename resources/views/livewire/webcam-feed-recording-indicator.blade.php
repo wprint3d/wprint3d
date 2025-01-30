e576ab7268b4c36609d136ff275b17be203c11e1
FacuM <facumo.fm@gmail.com>
2025-01-30 22:57:33 +0000

    Remove unused Livewire components and assets
    Deletes multiple Livewire components and related assets that are no longer in use. This cleanup reduces codebase complexity and improves maintainability.

--- resources/views/livewire/webcam-feed-recording-indicator.blade.php
+++
@@ -1,7 +0,0 @@
-<div>
-    @if ($camera->connected && $isRecording)
-        <div class="position-absolute text-danger bottom-0 end-0 fs-4 m-1 animate__animated animate__flash animate__infinite animate__slow">
-            @svg('record-fill', [ 'class' => 'opacity-50' ])
-        </div>
-    @endif
-</div>
--- resources/views/login.blade.php
+++
@@ -1,5 +0,0 @@
-@extends('common.page')
-
-@section('main')
-<livewire:login />
-@endsection
