435eb28bef4ebac653aa0806d734df26ea236238
FacuM <facumo.fm@gmail.com>
2025-02-12 01:04:38 +0000

    Remove device variants updater logging and scheduling
    - Eliminates the daily scheduling of the device variants refresh command.
    
    - Removes associated logging configuration to streamline logging processes.
    
    This change helps reduce unnecessary log clutter and improves overall application performance.

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,7 +15,6 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('refresh:device-variants')->daily();
     }
 
     /**
