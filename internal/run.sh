e58fec10a984de54ec419f4e001169d2b000f9b9
FacuM <facumo.fm@gmail.com>
2025-04-13 07:10:00 -0300

    fix(run.sh): streamline camera configuration handling and improve logging

--- internal/run.sh
+++ internal/run.sh
@@ -263,8 +263,8 @@ else
         if [[ "$ROLE" == 'server' ]]; then
             refreshDockerLog &
 
-            # Reset proxy configuration
-            truncate --size 0 /var/www/proxy/internal/recordings.conf /var/www/proxy/internal/cameras.conf;
+            # Reset proxy configuration for the recordings
+            truncate --size 0 /var/www/proxy/internal/recordings.conf;
 
             # Disable permissions checks for the Git repository
             git config --global --add safe.directory /var/www;
@@ -278,9 +278,6 @@ else
             if [[ $? -ne 0 ]]; then
                 exit 1; # crash and wait for self-restart
             fi;
-
-            # Reset proxy configuration
-            truncate --size 0 /var/www/proxy/internal/*.conf;
 
             # Flush cached files
             php artisan optimize:clear;
@@ -552,7 +549,9 @@ else
             # Here, we're redirecting any relevant information to stderr (>&2)
             # instead of stdout, as stdout has been trapped by this function.
             updateCameras() {
-                truncate --size 0 /tmp/cameras.conf;
+                TMP_CAMERAS_CONF=$(mktemp);
+
+                truncate --size 0 "$TMP_CAMERAS_CONF";
 
                 IFS=$'\n';
 
@@ -578,6 +577,15 @@ else
                     eval "$var";
 
                     if [[ "$CURRENT_ID" != '' ]] && ([[ "$_ID" != "$CURRENT_ID" ]] || [[ "$CURRENT_LINE" -eq "$MAX_LINE" ]]); then
+                        port='';
+                        FSWEBCAM_PID='';
+
+                        echo "ENABLED = $ENABLED" >&2;
+                        echo "NODE = $NODE" >&2;
+                        echo "SUPPORTS_MJPEG = $SUPPORTS_MJPEG" >&2;
+                        echo "port = $port" >&2;
+                        echo "FSWEBCAM_PID = $FSWEBCAM_PID" >&2;
+
                         echo "Refreshing device at node $NODE with ID $CURRENT_ID" >&2;
 
                         # camera-streamer doesn't like the actual full path
@@ -597,21 +605,23 @@ else
                         fi;
 
                         if [[ "$ENABLED" -eq 1 ]] && [[ -e "$NODE" ]]; then
-                            if [[ "$SUPPORTS_MJPEG" -eq 0 ]] && [[ "$REQUIRES_LIB_CAMERA" -eq 0 ]] && [[ "$FSWEBCAM_PID" == '' ]]; then
-                                echo "Starting fswebcam for $NODE" >&2;
+                            if [[ "$SUPPORTS_MJPEG" -eq 0 ]]; then
+                                if [[ "$REQUIRES_LIB_CAMERA" -eq 0 ]] && [[ "$FSWEBCAM_PID" == '' ]]; then
+                                    echo "Starting fswebcam for $NODE" >&2;
 
-                                fswebcam \
-                                    --device "$NODE" \
-                                    --no-banner \
-                                    --resolution "$RESOLUTION" \
-                                    --fps 1 \
-                                    --save /tmp/video/stream_"$CURRENT_ID".jpg \
-                                    --loop 1 &
-                                    # --quiet \
+                                    fswebcam \
+                                        --device "$NODE" \
+                                        --no-banner \
+                                        --resolution "$RESOLUTION" \
+                                        --fps 1 \
+                                        --loop 1 \
+                                        --quiet \
+                                        --save /tmp/video/stream_"$CURRENT_ID".jpg &
 
-                                echo "fswebcam started with PID $!" >&2;
+                                    echo "fswebcam started with PID $!" >&2;
 
-                                FSWEBCAM_PID=$!;
+                                    FSWEBCAM_PID=$!;
+                                fi;
                             elif [[ $LIB_CAMERA_UVC_PID == '' ]] && [[ "$LIB_CAMERA_CSI_PID" == '' ]]; then # not yet started
                                 port=$(getFreePort);
 
@@ -664,8 +674,6 @@ else
                                     fi;
                                 fi;
                             else
-                                port='';
-
                                 if [[ "$LIB_CAMERA_UVC_PID" != '' ]]; then
                                     echo "PID UVC: ${LIB_CAMERA_UVC_PID}" >&2;
 
@@ -690,15 +698,16 @@ else
                                     PROXY_PREFIX='csi';
                                 fi;
 
-                                printf "\nlocation /video/$MACHINE_UUID/$PROXY_PREFIX/$INDEX {" >> /tmp/cameras.conf;
-                                printf "\n\tproxy_pass            http://streamer:${port}/;"    >> /tmp/cameras.conf;
-                                printf "\n\tproxy_set_header Host \$host;"                      >> /tmp/cameras.conf;
-                                printf "\n\tinclude               nginxconfig.io/proxy.conf;"   >> /tmp/cameras.conf;
-                                printf "\n}"                                                    >> /tmp/cameras.conf;
+                                printf "\nlocation /video/$MACHINE_UUID/$PROXY_PREFIX/$INDEX {" >> $TMP_CAMERAS_CONF;
+                                printf "\n\tproxy_pass            http://streamer:${port}/;"    >> $TMP_CAMERAS_CONF;
+                                printf "\n\tproxy_set_header Host \$host;"                      >> $TMP_CAMERAS_CONF;
+                                printf "\n\tinclude               nginxconfig.io/proxy.conf;"   >> $TMP_CAMERAS_CONF;
+                                printf "\n}"                                                    >> $TMP_CAMERAS_CONF;
                             elif [[ "$FSWEBCAM_PID" != '' ]]; then
-                                printf "\nlocation /video/$MACHINE_UUID/$PROXY_PREFIX/$INDEX {" >> /tmp/cameras.conf;
-                                printf "\n\talias         /tmp/video/stream_${CURRENT_ID}.jpg;" >> /tmp/cameras.conf;
-                                printf "\n}"                                                    >> /tmp/cameras.conf;
+                                printf "\nlocation /video/$MACHINE_UUID/$PROXY_PREFIX/$INDEX {"  >> $TMP_CAMERAS_CONF;
+                                printf "\n\talias         /tmp/video/stream_${CURRENT_ID}.jpg;"  >> $TMP_CAMERAS_CONF;
+                                printf "\n\tadd_header    Content-Type image/jpeg;"              >> $TMP_CAMERAS_CONF;
+                                printf "\n}"                                                     >> $TMP_CAMERAS_CONF;
                             fi;
                         else # the camera has been disabled, kill and de-allocate resources
                             if [[ "$LIB_CAMERA_UVC_PID" != '' ]] || [[ "$LIB_CAMERA_CSI_PID" != '' ]] || [[ "$FSWEBCAM_PID" != '' ]]; then
@@ -731,24 +740,21 @@ else
                 rm -fv /var/www/internal/.requires_camera_detection;
 
                 CURRENT_SUM="$(md5sum /var/www/proxy/internal/cameras.conf | cut -d ' ' -f 1)"
-                NEW_SUM="$(md5sum /tmp/cameras.conf | cut -d ' ' -f 1)";
+                NEW_SUM="$(md5sum "$TMP_CAMERAS_CONF" | cut -d ' ' -f 1)";
 
                 if [[ "$CURRENT_SUM" != "$NEW_SUM" ]]; then
                     echo "Proxy server change detected, reloading... CSUM = ${CURRENT_SUM}, NSUM = ${NEW_SUM}" >&2;
 
-                    cp -fv /tmp/cameras.conf /var/www/proxy/internal/cameras.conf >&2;
+                    cp -fv "$TMP_CAMERAS_CONF" /var/www/proxy/internal/cameras.conf >&2;
+                    rm -fv "$TMP_CAMERAS_CONF" >&2;
 
                     for container_id in $(docker ps --filter name=proxy --format '{{ .ID }}'); do
                         docker exec -t $container_id nginx -s reload;
                     done;
+                else
+                    echo "No changes detected in the cameras.conf file (${CURRENT_SUM} == ${NEW_SUM})" >&2;
                 fi;
             }
-
-            if [[ ! -e '/var/www/proxy/internal/cameras.conf' ]]; then
-                truncate --size 0 /var/www/proxy/internal/cameras.conf;
-            fi;
-
-            truncate --size 0 /tmp/cameras.conf;
 
             updateCameras;
 
