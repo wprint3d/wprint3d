a6568707780be77a06bc98688c144cf33ceb7332
FacuM <facumo.fm@gmail.com>
2025-04-14 05:22:05 +0000

    Revert "De-duplicate code and memory by bundling the WebSocket server to the backend service"
    This reverts commit 983b088b8348bf1c5bfb951618bc43eec22d4679.

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -36,7 +36,7 @@ return [
             'secret'    => env('PUSHER_APP_SECRET'),
             'app_id'    => env('PUSHER_APP_ID', 'wp3d'),
             'options'   => [
-                'host'      => 'backend',
+                'host'      => 'ws-server',
                 'port'      => 6001,
                 'scheme'    => 'http',
                 'encrypted' => false,
