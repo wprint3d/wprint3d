3e1f01ce27bc25a3f425e3f69ec470f0a3e2e9ad
FacuM <facumo.fm@gmail.com>
2026-03-17 01:54:00 -0300

    docs: add custom CSS styling to documentation

--- docs/conf.py
+++ docs/conf.py
@@ -23,4 +23,5 @@ myst_fence_as_directive = ["mermaid"]
 
 html_title = "WPrint 3D Documentation"
 html_theme = "sphinx_rtd_theme"
-html_static_path = []
+html_static_path = ["_static"]
+html_css_files = ["custom.css"]
